8a5f96b9494774415d2016208019260b6c491ad1
David-Moisan <dmoisan.23@gmail.com>
2022-11-15 18:26:51 +0100

    Fix: Delete GUI for prod version

--- src/script.js
+++ src/script.js
@@ -3,13 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
-import * as dat from 'lil-gui'
 
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
